1cd5c3bbf339926ae8d97f1d963dec8a543c8b26
PAVAN467 <53818247+PAVAN467@users.noreply.github.com>
2019-08-07 12:56:29 +0530

    Add files via upload

---
+++ LAB0.py
@@ -0,0 +1,26 @@
+class A():
+    b=[]
+    def __init__(self,b,a):
+        self.b=b
+        self.a=a
+        
+    def finds(self):
+        for i in range(0,a):
+            c=int(input("enter elements"))
+            self.b.append(c)
+            print(self.b)
+
+    def max1(self):
+        m=0
+        for i in range(0,a):
+            if self.b[i]>m :
+                m=self.b[i]
+        print(m)
+                
+
+a=int(input("enter the number of elements to enter"))
+c=A([],a)
+c.finds()
+c.max1()
+            
+    
