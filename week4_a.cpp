35c3edb37d11cfc9b31ede74e0c3a543e525a30a
PAVAN467 <53818247+PAVAN467@users.noreply.github.com>
2019-10-07 20:41:13 +0530

    Update week4_a.cpp

--- week4_a.cpp
+++ week4_a.cpp
@@ -1,14 +1,13 @@
-#include<iostream>
-using namespace std;
 
+#include<stdio.h>
+#include<stdlib.h>
 int adjacent[20][20];
 int visited[20];
 int n;
 void DFS(int vtx)
 {
     int i;
-    cout<<vtx;
-    //printf("%d ",vtx);
+    printf("%d ",vtx);
     visited[vtx]=1;
     for(i=0;i<n;i++)
     {
@@ -25,7 +24,7 @@ void connected()
         if(!visited[i])
         {
             DFS(i);
-            cout<<"\n";
+            printf("\n");
         }
     }
 }
@@ -33,59 +32,13 @@ void connected()
 int main()
 {   
     int i,j;
-     cout<<"enter the adjacent matrix"<<"\n";
- cin>>n;
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<n;j++)#include<iostream>
-using namespace std;
-
-int adjacent[20][20];
-int visited[20];
-int n;
-void DFS(int vtx)
-{
-    int i;
-    cout<<vtx;
-    //printf("%d ",vtx);
-    visited[vtx]=1;
-    for(i=0;i<n;i++)
-    {
-        if(adjacent[vtx][i] && !visited[i])
-            DFS(i);
-    }
-}
-
-void connected()
-{
-    int i;
-    for(i=0;i<n;i++)
-    {
-        if(!visited[i])
-        {
-            DFS(i);
-            cout<<"\n";
-        }
-    }
-}
-
-int main()
-{   
-    int i,j;
-     cout<<"enter the adjacent matrix"<<"\n";
- cin>>n;
+    //printf("n : ");
+    scanf("%d",&n);//printf("Enter the adjacent matrix : ");
     for(i=0;i<n;i++)
     {
         for(j=0;j<n;j++)
         {
-            cin>>adjacent[i][j];
-        }
-        visited[i]=0;
-    }
-    connected();
-}
-        {
-            cin>>adjacent[i][j];
+            scanf("%d",&adjacent[i][j]);
         }
         visited[i]=0;
     }
